8a1178342acdc6ea7cdba2af91ca012fe52ba5d2
denten <denten@gmail.com>
2017-04-11 22:05:22 -0400

    delete empty line

--- aws-backup.sh
+++ aws-backup.sh
@@ -6,4 +6,3 @@ echo "last backup attempt on $stamp" > $logpath
 wall "Commencing AWS S3 backup from ~/bin/aws-backup.sh. For log files see ~/log/backup.log"
 
 aws s3 sync ~/gDrive s3://darkstar-backup --exclude "*.git/*" >> $logpath
-
