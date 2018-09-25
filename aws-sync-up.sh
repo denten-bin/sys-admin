a30d7f426f5fdeb5228d1e350ac33c3c4fbf7b8a
denten <denten@gmail.com>
2018-09-24 20:17:13 -0400

    be careful with which one is up and which one is down

---
+++ aws-sync-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+logpath=/home/denten/logs/backup.log
+stamp="$(date)"
+
+echo "last backup attempt on $stamp" > $logpath
+wall "Commencing AWS S3 backup from ~/bin/aws-backup.sh. For log files see ~/log/backup.log"
+
+#aws s3 sync s3://darkstar-backup ~/Documents --exclude "*.git/*" >> $logpath
+aws s3 sync ~/Documents s3://darkstar-backup >> $logpath
